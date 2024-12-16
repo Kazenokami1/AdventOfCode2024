28bac1352fe18565ee9fe3adeff69484a2403d1d
Kazenokami1 <kazenokamii@gmail.com>
2024-12-15 20:36:40 -0600

    Day 12 changed the wording on the comment.

--- Go Solutions/days/day12.go
+++ Go Solutions/days/day12.go
@@ -9,10 +9,13 @@ import (
 )
 
 /*
-	As a note, Part 2 of this will return the wrong cost if a group fully contained a group of plots that also fully contained another group of plots.
-	A fix for this would be to move the currently recursive function to a separate function that basically didn't check for an internal group.
-	In other words, copy the labyrinthRobotFunction, and cut lines 121-133 from that second function.
-	The first call (76) would still call labyrinthRobotFunction, the 2nd call (127) would call the new function instead.
+	As a note, Part 2 of this will return the wrong cost if a group fully contained more than one group within the same contained grouping of plots.
+	The fix for this would be to basically flip the robot so it runs a path around the inside of any hole that is found the same way it runs the
+	path around the outside right now.
+
+	In other words, pick a square and start inside the area, then check on your left for the wall instead of the right, and turn left when you move into
+	a space where the wall is no longer on your left, and turn right if you would run into a wall. You also need to take into account that you need to check
+	left first as you could have a 1 space opening on your left and have a wall directly in front of you (similar to how it is done that I check after I move).
 */
 
 func Day12() {
@@ -103,7 +106,6 @@ func labyrinthRobotFunction(group []*farmPlot, plotGroups map[[2]int][]*farmPlot
 	}
 	var returnedToStartingPosition bool
 	for !returnedToStartingPosition {
-		//Check to see if block in front of us is in group
 		if slices.Contains(group, farm[[2]int{labyrinthRobot.position[0] + labyrinthRobot.moveDirections[labyrinthRobot.facing%4][0], labyrinthRobot.position[1] + labyrinthRobot.moveDirections[labyrinthRobot.facing%4][1]}]) {
 			labyrinthRobot.facing--
 			turns++
@@ -115,7 +117,6 @@ func labyrinthRobotFunction(group []*farmPlot, plotGroups map[[2]int][]*farmPlot
 				turns++
 			}
 		}
-		//Check to see if we have a block to our right
 		returnedToStartingPosition = (labyrinthRobot.position == robotStartingPosition)
 	}
 	for _, plot := range group {
